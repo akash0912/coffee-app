966abc5a66d0d037155da6223535fff0b4bb4a75
Akash Lakhani <akashlakhani91200@gamil.com>
2022-03-09 14:50:51 +0530

    Changed all action and reducers
    Made changes like updating cart data, order reducers to accomodate the new data

--- store/reducer/Product.js
+++ store/reducer/Product.js
@@ -1,18 +1,56 @@
-import Coffee from '../../models/Coffee';
-import { SET_PRODUCTS } from '../actions/Product';
+import Coffee from "../../models/Coffee";
+import {
+  SET_PRODUCTS,
+  ADD_PRODUCTS,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+} from "../actions/Product";
 const initialState = {
-    availableProducts : []
-}
-export default (state = initialState, action)=>{
-    switch(action.type){
-        case SET_PRODUCTS:
-            return {
-                availableProducts : action.product
-            }
-        default:
-            return{
+  availableProducts: [],
+};
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PRODUCTS:
+      let temp = [...state.availableProducts];
+      temp.push(action.product);
+      return {
+        ...state,
+        availableProducts: temp,
+      };
+    case UPDATE_PRODUCT:
+      const selectedProductIndex = state.availableProducts.findIndex(
+        (item) => item._id === action.id
+      );
+      
+      const updatedProduct = new Coffee(
+        action.id,
+        action.product.name,
+        action.product.image,
+        action.product.ingredients,
+        action.product.price
+      );
 
-                initialState
-            }
-    }
-}
+      const updatedNewProducts = { ...state.availableProducts };
+      updatedNewProducts[selectedProductIndex] = updatedProduct;
+      return {
+        ...state,
+        availableProducts: updatedNewProducts,
+      };
+    case SET_PRODUCTS:
+
+      return {
+        availableProducts: action.product,
+      };
+    case DELETE_PRODUCT:
+      const productsAfterDeletion = state.availableProducts.filter(prod=>prod.id === action.id)
+      
+      return{
+        availableProducts: productsAfterDeletion
+      }
+    default:
+      return {
+        initialState,
+      };
+  }
+  return state;
+};
